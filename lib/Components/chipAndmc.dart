7a0d8acd8af991a145bee5dcd4f9966ba07c454a
saganorenkarlson <sagno626@student.liu.se>
2022-11-16 09:20:56 +0100

    Display numbers and card type

--- lib/Components/chipAndmc.dart
+++ lib/Components/chipAndmc.dart
@@ -2,61 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:lab2/theme/app_theme.dart';
 
 class chipAndmc extends StatefulWidget {
-  
- // var showfull =false;
-  chipAndmc( {Key? key}) : super(key: key);
+  // var showfull =false;
+  var cardType;
+
+  chipAndmc(this.cardType, {Key? key}) : super(key: key);
 
   @override
   State<chipAndmc> createState() => _chipAndmcState();
 }
 
 class _chipAndmcState extends State<chipAndmc> {
- var showfull = false; 
- 
+  var showfull = false;
 
   Widget buildItems(BuildContext context) {
- 
-  return Row(
-    mainAxisSize: MainAxisSize.max,
+    return Row(
+      mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  
-children: [
-  Container(
- height: 50,
- width: 65,
- margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-       decoration:
-       BoxDecoration(      
-                      borderRadius: BorderRadius.circular(10),
-                         
-                      image: const DecorationImage(
-                      image: AssetImage('assets/chip.png'),
-                    fit: BoxFit.cover,
-                    ),
-       ),),   Container(
- height: 50,
- width: 64.1,
- margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-       decoration:
-       const BoxDecoration(      
-                         
-                      image: DecorationImage(
-                      image: AssetImage('assets/mastercard.png'),
-                    fit: BoxFit.cover,
-                    ),
-       ),), 
- ],
-
-    );
-    
-}
-    @override
-  Widget build(BuildContext context) {
-    return(
-        buildItems(context)
+      children: [
+        Container(
+          height: 50,
+          width: 65,
+          margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: const DecorationImage(
+              image: AssetImage('assets/chip.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          height: 60,
+          width: 80,
+          margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/${widget.cardType}.png'),
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+        ),
+      ],
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return (buildItems(context));
+  }
 }
-
-
-
